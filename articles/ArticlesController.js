944727f54bdace6c84a0c6400dcb53cc7cb7105d
Lucas Faria <luksfaria12@outlook.com>
2021-02-02 20:46:05 -0300

    Final pagination

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -90,11 +90,14 @@ router.get("/articles/page/:num",(req,res)=> {
     if(isNaN(page) || page == 1){
         offset = 0
     }else {
-        offset = parseInt(page) * 4
+        offset = (parseInt(page) - 1) * 4
     }
     Article.findAndCountAll({
         limit: 4,
-        offset: offset   // retorna a partir de um artigo
+        offset: offset ,
+        order:[
+            ["id", "desc"]
+        ]  // retorna a partir de um artigo
     }).then((articles)=> {
 
         let next;
@@ -105,11 +108,15 @@ router.get("/articles/page/:num",(req,res)=> {
         }
 
         let result  = {
+            page : parseInt(page),
             next: next,
             articles: articles
 
         }
-        res.json(result)
+
+        Category.findAll().then((categories)=> {
+            res.render("admin/articles/page", {result: result, categories: categories})
+        })
     })
 })
 module.exports = router
--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/", CategoriesController)
 app.use("/", ArticlesController)
 
 app.get("/", (req,res)=> {
-    Article.findAll({order: [["id", "desc"]]}).then((articles)=> {
+    Article.findAll({order: [["id", "desc"]],limit: 4}).then((articles)=> {
         Category.findAll().then((categories)=> {
             res.render("index", {articles: articles, categories: categories})
         })
