8fa420508ba75103858360208f0d8e4bac97b82f
Lucas Faria <luksfaria12@outlook.com>
2021-02-01 22:17:45 -0300

    Pagination

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -82,4 +82,34 @@ router.post("/articles/update", (req,res)=> {
         res.redirect("/")
     })
 })
+
+router.get("/articles/page/:num",(req,res)=> {
+    let page = req.params.num
+    let offset = 0
+
+    if(isNaN(page) || page == 1){
+        offset = 0
+    }else {
+        offset = parseInt(page) * 4
+    }
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset   // retorna a partir de um artigo
+    }).then((articles)=> {
+
+        let next;
+        if(offset + 4 >= articles.count){
+            next = false
+        }else {
+            next = true
+        }
+
+        let result  = {
+            next: next,
+            articles: articles
+
+        }
+        res.json(result)
+    })
+})
 module.exports = router
